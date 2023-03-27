c9e4e483025cd5f77fbd2b8ed9aec7c58a544919
Tomasz <tomasz.iwanicki@onet.pl>
2023-03-27 17:33:00 +0200

    Fix error with data links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
         </a>
       </div>
       <ul className="hidden lg:flex justify-center items-center gap-8">
-        {data.map((link) => (
+        {data.navLinks.map((link) => (
           <a href={link.id} key={link.id}>
             <li className="hover:text-secondary smallcaps" onClick={() => setToggleMenu(false)}>
               {link.text}
@@ -27,7 +27,7 @@ const Navbar = () => {
       {toggleMenu && (
         <div className="absolute top-[70px] left-[30px] bg-lightGray z-5 rounded-md">
           <ul className="flex flex-col justify-center items-center gap-4 last:pb-4 first:pt-4">
-            {data.map((link) => (
+            {data.navLinks.map((link) => (
               <a href={link.id} key={link.id}>
                 <li className="hover:text-secondary smallcaps px-6 py-2 text-[18px]" onClick={() => setToggleMenu(false)}>
                   {link.text}
