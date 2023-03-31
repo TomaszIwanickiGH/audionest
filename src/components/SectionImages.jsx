45781faac1d1651276494bb9ffc9825d8922ef8f
Tomasz <tomasz.iwanicki@onet.pl>
2023-03-31 18:36:48 +0200

    Add images to SectionImages section

--- src/components/SectionImages.jsx
+++ src/components/SectionImages.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { styles, images } from '../constants'
 
 const SectionImages = () => {
-  return <div>SectionImages</div>
+  return (
+    <section className={`${styles.padding} mx-auto flex lg:flex-row flex-col justify-center gap-6 mt-4`}>
+      <div className="flex flex-col gap-6">
+        <img src={images.Hman} alt="man" className="rounded-md" />
+        <img src={images.Hlaptop} alt="laptop" className="rounded-md" />
+      </div>
+      <img src={images.Hmain} alt="main" className="w-[800px] rounded-md" />
+    </section>
+  )
 }
 
 export default SectionImages
