1c67f740dde8bb26d6e75cab9d1a2cef354030d1
Tomasz <tomasz.iwanicki@onet.pl>
2023-03-31 19:53:48 +0200

    Complete whole headphones section

--- src/components/HeadphonesWrapper.jsx
+++ src/components/HeadphonesWrapper.jsx
@@ -1,14 +1,68 @@
 import React, { useState } from 'react'
 import { MainProduct, Devices, MainDescription, SectionImages } from '../components'
-import { images } from '../constants'
+import { images, styles } from '../constants'
 
 const HeadphonesWrapper = () => {
-  const [currentProduct, setCurrentProduct] = useState({ price: '$ 399.99', image: images.Hmain })
+  const [currentProduct, setCurrentProduct] = useState({ price: '$ 399.99', image: images.Hmain, title: 'XX99 MARK II' })
   return (
     <section>
-      <MainProduct image={currentProduct.image} price={currentProduct.price} />
+      <MainProduct image={currentProduct.image} price={currentProduct.price} title={currentProduct.title} />
       <MainDescription />
       <SectionImages />
+
+      <div className={`${styles.padding} mx-auto flex flex-col mb-[-4rem]`}>
+        <h2 className="text-primary text-center text-[36px] font-[500]">YOU MAY ALSO LIKE</h2>
+        <div className="flex lg:flex-row flex-col items-center justify-between mt-16 gap-4">
+          <div className="flex flex-col gap-6 items-center">
+            <div className="bg-veryVeryLightGray w-[300px] h-[300px] flex justify-center items-center">
+              <img src={images.HEX1} alt="hex1" className="w-[230px] px-4 py-6 rounded-md" />
+            </div>
+            <h3 className="text-primary font-[500] text-[24px]">XX99 MARK I</h3>
+            <button
+              className="bg-secondary px-6 py-3 lg:w-[180px] w-[150px] text-white hover:text-primary hover:font-[500] lg-mb-0 mb-8 lg:text-[16px] text-[14px]"
+              onClick={() => {
+                window.scrollTo(0, 0)
+                setCurrentProduct({ price: '$ 289.99', image: images.HEX1, title: 'XX MARK I' })
+              }}
+            >
+              See Product
+            </button>
+          </div>
+
+          <div className="flex flex-col gap-6 items-center">
+            <div className="bg-veryVeryLightGray w-[300px] h-[300px] flex justify-center items-center">
+              <img src={images.HEX2} alt="hex2" className="w-[230px] px-4 py-6 rounded-md" />
+            </div>
+            <h3 className="text-primary font-[500] text-[24px]">XX59</h3>
+            <button
+              className="bg-secondary px-6 py-3 lg:w-[180px] w-[150px] text-white hover:text-primary hover:font-[500] lg-mb-0 mb-8 lg:text-[16px] text-[14px]"
+              onClick={() => {
+                window.scrollTo(0, 0)
+                setCurrentProduct({ price: '$ 349.99', image: images.HEX2, title: 'XX59' })
+              }}
+            >
+              See Product
+            </button>
+          </div>
+
+          <div className="flex flex-col gap-6 items-center">
+            <div className="bg-veryVeryLightGray w-[300px] h-[300px] flex justify-center items-center">
+              <img src={images.HEX3} alt="hex3" className="w-full px-4 py-6 rounded-md" />
+            </div>
+            <h3 className="text-primary font-[500] text-[24px]">X21 GEN 7</h3>
+            <button
+              className="bg-secondary px-6 py-3 lg:w-[180px] w-[150px] text-white hover:text-primary hover:font-[500] lg-mb-0 mb-8 lg:text-[16px] text-[14px]"
+              onClick={() => {
+                window.scrollTo(0, 0)
+                setCurrentProduct({ price: '$ 299.99', image: images.HEX3, title: 'X21 GEN 7' })
+              }}
+            >
+              See Product
+            </button>
+          </div>
+        </div>
+      </div>
+
       <Devices />
     </section>
   )
