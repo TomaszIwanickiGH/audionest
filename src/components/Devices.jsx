61e52d68882f70100c89cd2b80fd039eddfd11f7
Tomasz <tomasz.iwanicki@onet.pl>
2023-04-01 18:01:01 +0200

    Add Link tags to make routing work properly

--- src/components/Devices.jsx
+++ src/components/Devices.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { data, styles } from '../constants'
+import { Link } from 'react-router-dom'
 
 const Devices = () => {
   const { devices } = data
@@ -9,7 +10,11 @@ const Devices = () => {
         <img src={devices[0].image} alt="title" className={`${devices[0].className} absolute top-[-60px]`} />
         <h3 className="mt-28 font-[600] tracking-[.06rem]">{devices[0].title}</h3>
         <div className="mt-3 flex items-center justify-center gap-2">
-          <p className="text-[13px] font-[700] hover:text-veryLightGray hover:cursor-pointer text-secondary">{devices[0].text}</p>
+          <p className="text-[13px] font-[700] hover:text-veryLightGray hover:cursor-pointer text-secondary">
+            <Link to="/headphones" onClick={() => window.scrollTo(0, 0)}>
+              {devices[0].text}
+            </Link>
+          </p>
           <img src={devices[0].arrow} alt="arrow" className="w-[11px] h-[11px]" />
         </div>
       </div>
@@ -18,7 +23,11 @@ const Devices = () => {
         <img src={devices[1].image} alt="title" className={`${devices[1].className} absolute top-[-60px]`} />
         <h3 className="mt-28 font-[600] tracking-[.06rem]">{devices[1].title}</h3>
         <div className="mt-3 flex items-center justify-center gap-2">
-          <p className="text-[13px] font-[700] hover:text-veryLightGray hover:cursor-pointer text-secondary">{devices[1].text}</p>
+          <p className="text-[13px] font-[700] hover:text-veryLightGray hover:cursor-pointer text-secondary">
+            <Link to="/speakers" onClick={() => window.scrollTo(0, 0)}>
+              {devices[1].text}
+            </Link>
+          </p>
           <img src={devices[1].arrow} alt="arrow" className="w-[11px] h-[11px]" />
         </div>
       </div>
@@ -27,7 +36,11 @@ const Devices = () => {
         <img src={devices[2].image} alt="title" className={`${devices[2].className} absolute top-[-60px]`} />
         <h3 className="mt-28 font-[600] tracking-[.06rem]">{devices[2].title}</h3>
         <div className="mt-3 flex items-center justify-center gap-2">
-          <p className="text-[13px] font-[700] hover:text-veryLightGray hover:cursor-pointer text-secondary">{devices[2].text}</p>
+          <p className="text-[13px] font-[700] hover:text-veryLightGray hover:cursor-pointer text-secondary">
+            <Link to="/earphones" onClick={() => window.scrollTo(0, 0)}>
+              {devices[2].text}
+            </Link>
+          </p>
           <img src={devices[2].arrow} alt="arrow" className="w-[11px] h-[11px]" />
         </div>
       </div>
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { styles, images } from '../constants'
+import { Link } from 'react-router-dom'
 
 const Products = () => {
   return (
@@ -9,23 +10,35 @@ const Products = () => {
         <div className="flex flex-col justify-center lg:items-start items-center lg:w-[40%] w-[70%] gap-4 lg:text-start text-center">
           <h2 className="text-white text-[48px] font-[500]">ZX9 SPEAKER</h2>
           <p className="lg:text-[14px] text-[16px] text-veryVeryLightGray lg:w-[70%] w-full">Upgrade to premium spakers that are phenomenally built to deliver truly remarkable sound.</p>
-          <button className="bg-primary hover:bg-white lg:px-4 lg:py-2 px-8 py-4 lg:w-[150px] w-[180px] text-white hover:text-primary hover:font-[500] lg-mb-0 mb-8 lg:text-[16px] text-[14px] hover:outline-2 hover:outline-primary hover:outline mt-2">See Product</button>
+          <button className="bg-primary hover:bg-white lg:px-4 lg:py-2 px-8 py-4 lg:w-[150px] w-[180px] text-white hover:text-primary hover:font-[500] lg-mb-0 mb-8 lg:text-[16px] text-[14px] hover:outline-2 hover:outline-primary hover:outline mt-2">
+            <Link to="/speakers" onClick={() => window.scrollTo(0, 0)}>
+              See Product
+            </Link>
+          </button>
         </div>
       </div>
 
       <div className="w-full bg-veryVeryLightGray rounded-md flex lg:flex-row flex-col-reverse justify-around py-6 items-center gap-8">
         <div className="flex flex-col justify-center lg:items-start items-center">
-          <h2 className="text-primary lg:text-[28px] text-[32px] font-[500]">ZH7 SPEAKER</h2>
-          <button className="bg-veryVeryLightGray lg:px-4 lg:py-2 px-8 py-4 lg:w-[150px] w-[180px] text-primary  hover:font-[500] lg-mb-0 mb-8 lg:text-[16px] text-[14px] outline-2 outline-veryLightGray hover:outline-primary outline mt-4">See Product</button>
+          <h2 className="text-primary lg:text-[28px] text-[32px] font-[500]">XX99 MARK II</h2>
+          <button className="bg-veryVeryLightGray lg:px-4 lg:py-2 px-8 py-4 lg:w-[150px] w-[180px] text-primary  hover:font-[500] lg-mb-0 mb-8 lg:text-[16px] text-[14px] outline-2 outline-veryLightGray hover:outline-primary outline mt-4">
+            <Link to="/headphones" onClick={() => window.scrollTo(0, 0)}>
+              See Product
+            </Link>
+          </button>
         </div>
-        <img src={images.speakersLogo} alt="zh7" className="lg:w-[350px] lg:h-[225px] w-[300px] h-[192px]" />
+        <img src={images.HEX1} alt="zh7" className="lg:w-[350px] lg:h-[225px] w-[300px] h-[192px]" />
       </div>
 
       <div className="w-full rounded-md flex lg:flex-row flex-col lg:gap-8 gap-2">
         <img src={images.yx1Earphones} alt="yx1" className="rounded-md" />
         <div className="flex flex-col items-center justify-center bg-veryVeryLightGray w-full rounded-md pt-8">
-          <h2 className="text-primary lg:text-[28px] text-[32px] font-[500]">ZH7 SPEAKER</h2>
-          <button className="bg-veryVeryLightGray lg:px-4 lg:py-2 px-8 py-4 lg:w-[150px] w-[180px] text-primary  hover:font-[500] lg-mb-0 mb-8 lg:text-[16px] text-[14px] outline-2 outline-veryLightGray hover:outline-primary outline mt-4">See Product</button>
+          <h2 className="text-primary lg:text-[28px] text-[32px] font-[500]">EDIFIER X2</h2>
+          <button className="bg-veryVeryLightGray lg:px-4 lg:py-2 px-8 py-4 lg:w-[150px] w-[180px] text-primary  hover:font-[500] lg-mb-0 mb-8 lg:text-[16px] text-[14px] outline-2 outline-veryLightGray hover:outline-primary outline mt-4">
+            <Link to="/earphones" onClick={() => window.scrollTo(0, 0)}>
+              See Product
+            </Link>
+          </button>
         </div>
       </div>
     </section>
