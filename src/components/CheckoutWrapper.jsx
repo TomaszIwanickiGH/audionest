6b46ded8be6543eb12062a84a3a3839b009f45db
Tomasz <tomasz.iwanicki@onet.pl>
2023-04-03 19:11:47 +0200

    Start working on summary section

--- src/components/CheckoutWrapper.jsx
+++ src/components/CheckoutWrapper.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { styles } from '../constants'
+import { styles, images } from '../constants'
 import { Link } from 'react-router-dom'
 import { Form } from '../components'
 
@@ -12,6 +12,37 @@ const CheckoutWrapper = () => {
 
       <div className="flex lg:flex-row flex-col gap-8 lg:items-start items-center">
         <Form />
+        <div className="bg-white rounded-lg flex flex-col gap-4 sm:px-8 px-6 sm:py-8 py-6 mt-8 min-w-[400px]">
+          <h3 className="text-primary font-[500] text-[24px]">SUMMARY</h3>
+
+          <div className="flex gap-4">
+            <div className="flex justify-center items-center bg-veryVeryLightGray w-[80px] h-[80px] rounded-lg">
+              <img src={images.HEX1} alt="img" className="w-[65px] h-[55px]" />
+            </div>
+            <div className="flex flex-1 justify-between mt-2">
+              <div>
+                <p className="font-[600] text-[15px] text-primary">XX99 MK I</p>
+                <p className="font-[500] text-[14px] text-veryLightGray mt-2">$ 299.99</p>
+              </div>
+              <p className="font-[500] text-[15px] text-veryLightGray">x1</p>
+            </div>
+          </div>
+
+          <div className="flex justify-between">
+            <p className="text-[15px] text-veryLightGray">TOTAL</p>
+            <p className="text-[15px] text-primary font-[600]">$ 5,346</p>
+          </div>
+          <div className="flex justify-between">
+            <p className="text-[15px] text-veryLightGray">SHIPPING</p>
+            <p className="text-[15px] text-primary font-[600]">$ 20</p>
+          </div>
+          <div className="flex justify-between mt-2">
+            <p className="text-[15px] text-veryLightGray">GRAND TOTAL</p>
+            <p className="text-[15px] text-secondary font-[600]">$ 5,366</p>
+          </div>
+
+          <button className="bg-secondary px-6 py-3 w-full text-white hover:text-primary hover:font-[500] lg:text-[16px] text-[14px] mt-4">Continue & Pay</button>
+        </div>
       </div>
     </section>
   )
