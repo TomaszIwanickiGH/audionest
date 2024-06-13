37f7a8a4dc6262f30b036393016fcb5f4163ded4
Tomasz <tomasz.iwanicki@onet.pl>
2024-06-13 17:34:24 +0200

    Fix image

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,39 +1,39 @@
-import earphonesLogo from '../assets/earphones-logo.png'
-import headphonesLogo from '../assets/headphones-logo.png'
-import speakersLogo from '../assets/speakers-logo.png'
-import headphonesImage from '../assets/headphones-image.jpg'
-import manPhoto from '../assets/man-photo.jpg'
-import yx1Earphones from '../assets/yx1-earphones.jpg'
-import zh7Speakers from '../assets/zh7-speakers.jpg'
-import shoppingCart from '../assets/shopping-cart.png'
-import iconFacebook from '../assets/icon-facebook.svg'
-import iconHamburger from '../assets/icon-hamburger.svg'
-import iconInstagram from '../assets/icon-instagram.svg'
-import iconTwitter from '../assets/icon-twitter.svg'
-import iconArrow from '../assets/icon-arrow.svg'
-import iconClose from '../assets/icon-close.svg'
-import iconCloseBlack from '../assets/icon-close-black.svg'
-import speakersImage from '../assets/speakers-image.png'
+import earphonesLogo from '../assets/earphones-logo.png';
+import headphonesLogo from '../assets/headphones-logo.png';
+// import speakersLogo from '../assets/speakers-logo.png'
+import headphonesImage from '../assets/headphones-image.jpg';
+import manPhoto from '../assets/man-photo.jpg';
+import yx1Earphones from '../assets/yx1-earphones.jpg';
+import zh7Speakers from '../assets/zh7-speakers.jpg';
+import shoppingCart from '../assets/shopping-cart.png';
+import iconFacebook from '../assets/icon-facebook.svg';
+import iconHamburger from '../assets/icon-hamburger.svg';
+import iconInstagram from '../assets/icon-instagram.svg';
+import iconTwitter from '../assets/icon-twitter.svg';
+import iconArrow from '../assets/icon-arrow.svg';
+import iconClose from '../assets/icon-close.svg';
+import iconCloseBlack from '../assets/icon-close-black.svg';
+import speakersImage from '../assets/speakers-image.png';
 
-import HEX1 from '../assets/headphones/H-ex1.png'
-import HEX2 from '../assets/headphones/H-ex2.png'
-import HEX3 from '../assets/headphones/H-ex3.png'
-import Hlaptop from '../assets/headphones/H-laptop.jpg'
-import Hmain from '../assets/headphones/H-main.jpg'
-import Hman from '../assets/headphones/H-man.jpg'
+import HEX1 from '../assets/headphones/H-ex1.png';
+import HEX2 from '../assets/headphones/H-ex2.png';
+import HEX3 from '../assets/headphones/H-ex3.png';
+import Hlaptop from '../assets/headphones/H-laptop.jpg';
+import Hmain from '../assets/headphones/H-main.jpg';
+import Hman from '../assets/headphones/H-man.jpg';
 
-import SEX1 from '../assets/speakers/S-ex1.png'
-import SEX2 from '../assets/speakers/S-ex2.jpg'
-import SEX3 from '../assets/speakers/S-ex3.png'
-import SMain1 from '../assets/speakers/S-main1.jpg'
-import SMain2 from '../assets/speakers/S-main2.jpg'
+import SEX1 from '../assets/speakers/S-ex1.png';
+import SEX2 from '../assets/speakers/S-ex2.jpg';
+import SEX3 from '../assets/speakers/S-ex3.png';
+import SMain1 from '../assets/speakers/S-main1.jpg';
+import SMain2 from '../assets/speakers/S-main2.jpg';
 
-import EEX1 from '../assets/earphones/E-ex1.png'
-import EEX2 from '../assets/earphones/E-ex2.png'
-import EEX3 from '../assets/earphones/E-ex3.png'
-import EMain1 from '../assets/earphones/E-main1.jpg'
-import EMain2 from '../assets/earphones/E-main2.jpg'
-import EMain3 from '../assets/earphones/E-main3.jpg'
+import EEX1 from '../assets/earphones/E-ex1.png';
+import EEX2 from '../assets/earphones/E-ex2.png';
+import EEX3 from '../assets/earphones/E-ex3.png';
+import EMain1 from '../assets/earphones/E-main1.jpg';
+import EMain2 from '../assets/earphones/E-main2.jpg';
+import EMain3 from '../assets/earphones/E-main3.jpg';
 
 export default {
   earphonesLogo,
@@ -69,4 +69,4 @@ export default {
   EMain1,
   EMain2,
   EMain3,
-}
+};
